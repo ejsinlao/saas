e20af447445a780c9591649b06b5def2e6d97a96
Emmanuel Sinlao <sinlaoej@live.com>
2018-04-15 19:08:40 +0000

    added form validation to model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,4 +3,8 @@ class Contact < ActiveRecord::Base
     #name
     #email
     #comments
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :comments, presence: true
+    
 end
