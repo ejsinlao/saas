1ba6c0430567093999464e6a620d625950826e2b
Emmanuel Sinlao <sinlaoej@live.com>
2018-06-05 03:03:15 +0000

    Added server side code for creating subs for pro users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  #before action for any devise url where it will whitelist fields
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
+    #method to whitelist fields in form
+    def configure_permitted_parameters
+       devise_parameter_sanitizer.permit(:sign_up){ |u| u.permit(:stripe_card_token,:email,:password,:password_confirmation) } 
+    end
 end
